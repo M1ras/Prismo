0ae4626890b2b686717e07cb44dd6d743039884b
Miras Absar <miras@miras.io>
2018-07-31 17:31:47 -0700

    Implement help and version arguments.

--- prismo-grubby.sh
+++ prismo-grubby.sh
@@ -1,1 +1,35 @@
 #!/usr/bin/env bash
+
+NAME='Prismo'
+VERSION='0.0.0'
+
+OPTION_HELP='h'
+OPTION_VERSION='v'
+OPTIONS="$OPTION_HELP$OPTION_VERSION"
+
+DESCRIPTION_HELP='Help Description'
+DESCRIPTION_VERSION='Version Description'
+
+help() {
+  /usr/bin/printf ' -%s\t%s\n' \
+    "$OPTION_HELP" "$DESCRIPTION_HELP" \
+    "$OPTION_VERSION" "$DESCRIPTION_VERSION"
+
+  exit
+}
+
+version() {
+  /usr/bin/printf '%s %s\n' "$NAME" "$VERSION"
+  exit
+}
+
+while getopts "$OPTIONS" OPTION; do
+  case "$OPTION" in
+    "$OPTION_HELP")
+      help
+      ;;
+    "$OPTION_VERSION")
+      version
+      ;;
+  esac
+done
